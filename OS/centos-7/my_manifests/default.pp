453f5fea9a48179b6eea23992fa8281862ec1142
Robert Foster <robertfoster@MacBook-Air.home>
2019-08-17 12:56:29 +0100

    added LC_ALL env var to centos-7 build

--- OS/centos-7/my_manifests/default.pp
+++ OS/centos-7/my_manifests/default.pp
@@ -8,3 +8,8 @@ user { $users:
   ensure   => present,
   password => pw_hash('password', 'SHA-512', 'mysalt'),
 }
+
+file { '/etc/profile.d/lc_all.sh':
+  ensure  => file,
+  content => 'export LC_ALL=en_US.utf8',
+}
