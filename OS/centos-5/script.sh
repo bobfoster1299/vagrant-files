2e6d313796a67582d3f31b9b49fe05e1480691d1
Robert Foster <robertfoster@MacBook-Air.home>
2019-08-18 11:47:12 +0100

    updated script in centos-5

--- OS/centos-5/script.sh
+++ OS/centos-5/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rpm -Uvh https://yum.puppetlabs.com/puppet6/puppet-release-el-6.noarch.rpm
+rpm -Uvh http://yum.puppetlabs.com/puppet/el/5/x86_64/puppet6-release-6.0.0-4.el5.noarch.rpm
 yum install puppet-agent man man-pages telnet elinks vim-enhanced bash-completion -y
 /opt/puppetlabs/bin/puppet module install puppet-selinux --version 1.6.1
 rm -rf /etc/localtime
