577987318059518dec3860d1f88988399d45fe79
Robert Foster <robertfoster@centos8-1.lan>
2021-02-17 23:35:11 +0000

    remove puppet

--- OS/centos-8/script.sh
+++ OS/centos-8/script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-rpm -Uvh https://yum.puppetlabs.com/puppet6/puppet6-release-el-7.noarch.rpm
-yum install puppet-agent man-pages telnet elinks vim-enhanced bash-completion tmux -y
-/opt/puppetlabs/bin/puppet module install puppet-selinux --version 1.6.1
+yum install man-pages telnet vim-enhanced bash-completion tmux -y
 nmcli device connect eth1
 timedatectl set-timezone Europe/London
 systemctl enable firewalld && systemctl start firewalld
+cat <<EOF > /etc/profile.d/lc_all.sh
+export LC_ALL=en_US.utf8
+EOF
