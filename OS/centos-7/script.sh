e426c9de8ae0f661f99aed90e18571e669f39cda
Robert Foster <bobfoster1299@hotmail.co.uk>
2021-02-17 22:30:39 +0000

    Update script.sh

--- OS/centos-7/script.sh
+++ OS/centos-7/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rpm -Uvh https://yum.puppetlabs.com/puppet6/puppet-release-el-7.noarch.rpm
+rpm -Uvh https://yum.puppetlabs.com/puppet6/puppet6-release-el-7.noarch.rpm
 yum install puppet-agent man-pages telnet elinks vim-enhanced bash-completion tmux -y
 /opt/puppetlabs/bin/puppet module install puppet-selinux --version 1.6.1
 nmcli device connect eth1
