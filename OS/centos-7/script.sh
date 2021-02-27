de219c5b1fb3f786e2dacfa1bfe6575949e3d315
Robert Foster <bobfoster1299@hotmail.co.uk>
2021-02-27 20:30:45 +0000

    Update script.sh

--- OS/centos-7/script.sh
+++ OS/centos-7/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install man-pages telnet vim-enhanced bash-completion tmux -y
+yum install man-pages telnet vim-enhanced bash-completion -y
 timedatectl set-timezone Europe/London
 systemctl enable firewalld && systemctl start firewalld
 cat <<EOF > /etc/profile.d/lc_all.sh
